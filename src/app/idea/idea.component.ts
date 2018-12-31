59543537fb4dee13c837d0d8be538fb1907376d2
ivobar <babaginka@students.softuni.bg>
2018-12-31 18:37:20 +0200

    Added animation to idea page

--- src/app/idea/idea.component.ts
+++ src/app/idea/idea.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-idea',
   templateUrl: './idea.component.html',
-  styleUrls: ['./idea.component.scss']
+  styleUrls: ['./idea.component.scss'],
+  animations: [
+    trigger('easeIn', [
+      state('in', style({ opacity: 1 })),
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class IdeaComponent implements OnInit {
 
